59627fea19cca1b924ecf8325a08c557e1d4d318
kolotaev <kolotaev@tut.by>
2019-05-22 14:17:51 +0300

    Start experiments

---
+++ app/front.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+
+class Front:
+    def __init__(self, parent):
+        self.source = tk.Label(parent, padx=100, pady=50, text="Word!")
+        self.trans = tk.Message(parent, padx=100, background='grey', pady=50, text="Translated!")
+
+    def show(self):
+        self.source.pack()
+        self.trans.pack()
--- main.py
+++ main.py
@@ -1,7 +1,30 @@
-import tkinter
+import tkinter as tk
 
-top = tkinter.Tk()
+from app.front import Front
 
 
+def center(win):
+    win.update_idletasks()
+    width = win.winfo_width()
+    height = win.winfo_height()
+    x = (win.winfo_screenwidth() // 2) - (width // 2)
+    y = (win.winfo_screenheight() // 2) - (height // 2)
+    win.geometry('{}x{}+{}+{}'.format(width, height, x, y))
 
-top.mainloop()
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.geometry('800x600')
+    center(root)
+    # root.after(1, lambda: root.focus_force())
+    # root.focus_force()
+    root.lift()
+    root.attributes("-topmost", True)
+
+    def handle_focus(event):
+        if event.widget == root:
+            root.focus_set()
+
+    root.bind("<FocusIn>", handle_focus)
+    Front(root).show()
+    root.mainloop()
