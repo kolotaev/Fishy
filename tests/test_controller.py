3eea9dd6df08623782e805855cdb2c01938716ee
kolotaev <kolotaev@tut.by>
2019-06-18 20:37:27 +0300

    Fix tests

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -1,27 +1,26 @@
 from unittest import TestCase
-from unittest.mock import Mock
+from unittest.mock import Mock, PropertyMock
 
 from app.controller import Controller
-from app.view.root import MainFrame
 
 
 class TestController(TestCase):
     def test_start(self):
         model = Mock()
-        view = Mock(spec=MainFrame)
+        view = PropertyMock()
         showing_th = Mock()
-        cf = Controller(model, view, Mock)
-        cf.showing_thread = showing_th
-        cf.start()
+        ctrl = Controller(model, view, Mock())
+        ctrl.showing_thread = showing_th
+        ctrl.start()
         showing_th.start.assert_called_once()
         view.show.assert_called_once()
         view.loop.assert_called_once()
 
     def test_stop(self):
         model = Mock()
-        view = Mock(spec=MainFrame)
+        view = PropertyMock()
         showing_th = Mock()
-        cf = Controller(model, view, Mock)
-        cf.showing_thread = showing_th
-        cf.stop()
+        ctrl = Controller(model, view, Mock())
+        ctrl.showing_thread = showing_th
+        ctrl.stop()
         showing_th.terminate.assert_called_once()
---
+++ tests/test_words_model.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+from unittest.mock import Mock, patch
+
+from app.model.words import *
+from app.configurator import Config
+
+
+class TestWordsModel(TestCase):
+    @patch('app.model.words.CsvFileWords')
+    def test_create_model(self, csv_file_words_mock):
+        c = Mock()
+        create_model(c)
+        csv_file_words_mock.assert_called_once()
